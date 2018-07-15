ba6f2158de01152bd1b7aa0243d820fa6b239ee8
wchandu123 <chandu.dhaundiyal@gmail.com>
2018-07-15 21:44:20 +0530

    sql

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=127.0.0.1;dbname=cricket_tournament',
+	'connectionString' => 'mysql:host=127.0.0.1;dbname=cricket_league',
 	'emulatePrepare' => true,
 	'username' => 'cricketAdmin',
 	'password' => 'password',
